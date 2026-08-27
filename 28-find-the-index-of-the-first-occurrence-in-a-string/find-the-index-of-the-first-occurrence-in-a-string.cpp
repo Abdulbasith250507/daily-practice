f20a53aba7fb7e7e261172a7abcc77a4f1ab4729
Abdulbasith250507 <abasith250507@gmail.com>
2026-08-27 23:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (10.34%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,3 +1,5 @@
+// BRUTE FORCE APPROACH
+/*
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -13,6 +15,58 @@ public:
             }
             if(match)
             return i;
+        }
+        return -1;
+    }
+};
+*/
+
+// OPTIMIZED CODE USING KMP(KRUTH MORRIS PRATT) ALGORITHM
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        if (m > n)
+            return -1;
+        // Build LPS array
+        vector<int> lps(m, 0);
+        int len = 0;
+        int i = 1;
+        while (i < m) {
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        // KMP search
+        i = 0;
+        int j = 0;
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+            }
+            if (j == m)
+                return i - j;
+            if (i < n && haystack[i] != needle[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                }
+                else {
+                    i++;
+                }
+            }
         }
         return -1;
     }
