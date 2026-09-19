5d2ae693218dba30ab022af312091a8dfe617278
Abdulbasith250507 <abasith250507@gmail.com>
2026-09-19 19:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (64.86%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        long long reversedNum = 0;
+        int originalNum = x;
+
+        while (x > 0) {
+            reversedNum = reversedNum * 10 + x % 10;
+            x /= 10;
+        }
+
+        return originalNum == reversedNum;
+    }
+};
